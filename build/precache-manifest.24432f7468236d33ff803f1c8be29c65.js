cac820747163bfbd77e07b136fd2b52e54dab327
Jimmy Rowland <tian_yu@student.smc.edu>
2020-06-04 20:53:27 -0700

    relative path

---
+++ build/precache-manifest.24432f7468236d33ff803f1c8be29c65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8db1664b50d027f709bb7906680eb7dc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8b23e115bed3fd6a0155",
+    "url": "./static/css/main.6572b349.chunk.css"
+  },
+  {
+    "revision": "d59ceee04e02125784e5",
+    "url": "./static/js/2.51fb3b99.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.51fb3b99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b23e115bed3fd6a0155",
+    "url": "./static/js/main.a1429eb8.chunk.js"
+  },
+  {
+    "revision": "aa46abd20c0608b04db2",
+    "url": "./static/js/runtime-main.518bedc7.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "./static/media/logo.8dad8028.svg"
+  }
+]);
