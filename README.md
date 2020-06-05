35c61a2c2dec729ebfbcf6a8a96b5c1ae2022216
Jimmy Rowland <tian_yu@student.smc.edu>
2020-06-04 20:47:06 -0700

    add

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app), using the [Redux](https://redux.js.org/) and [Redux Toolkit](https://redux-toolkit.js.org/) template.
-
+## [DEMO](https://jimmyrowland.github.io/messageboardreduxOld/build/)
 ## Available Scripts
 
 In the project directory, you can run:
---
+++ build/precache-manifest.853f43b593dc1d0aefa5869df019d8c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd929cf14beccba2336ae2e2f1c6cda4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e120861018aad41b7604",
+    "url": "/static/css/main.6572b349.chunk.css"
+  },
+  {
+    "revision": "d59ceee04e02125784e5",
+    "url": "/static/js/2.51fb3b99.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.51fb3b99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e120861018aad41b7604",
+    "url": "/static/js/main.702fc035.chunk.js"
+  },
+  {
+    "revision": "20dcf7607374f6bbd4df",
+    "url": "/static/js/runtime-main.f8b90e4e.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/static/media/logo.8dad8028.svg"
+  }
+]);
